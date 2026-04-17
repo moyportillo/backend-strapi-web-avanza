86e67c980a6f66e84da6c9b0df944a9bfde69b4c
moyportillo <moyporti15@email.com>
2026-04-17 15:48:55 -0600

    finalizacion de update2

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path"
 
 export default defineConfig({
   plugins: [react()],
-  base: '/backend-strapi-web-avanza/',
+  base: '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
